65428d2bfbdd0a1bcac8258cfbc1f0cd734dd36f
mcurto <manuelantoniocurto@gmail.com>
2020-06-15 15:25:59 +0100

    Update CountLengths.bash

--- CountLengths.bash
+++ CountLengths.bash
@@ -2,9 +2,9 @@
 Inputdir=$1;
 Outputdir=$2
 
-for filename in $(ls $Outputdir/SeparatOut); do
-    awk 'NR%4 == 2 {lengths[length($0)]++} END {for (l in lengths) {print l, lengths[l]}}' $Outputdir/SeparatOut/$filename | sort -k2 -n  > $Outputdir/MarkerStatistics/$filename.statistics
+for filename in $(ls $Inputdir); do
+    awk 'NR%4 == 2 {lengths[length($0)]++} END {for (l in lengths) {print l, lengths[l]}}' $Inputdir/$filename | sort -k2 -n  > $Outputdir/$filename.statistics
     newname=$(echo $filename | awk 'BEGIN { FS = "_" }; { print $2"_"$3"_"$1"_" }' | sed 's/\.fastq//g' )      #FASTQ files need here fa
     echo $newname
-    mv $Outputdir/MarkerStatistics/$filename.statistics $Outputdir/MarkerStatistics/$newname.statistics
+    mv $Outputdir/$filename.statistics $Outputdir/$newname.statistics
 done
